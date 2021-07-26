d71eac7bbfccd34bfb0788233b39fdfb00f39461
Gevorg Martirosyan <gevorg.martirosyan.1998.08@gmail.com>
2021-07-26 04:22:36 +0400

    horible code

--- main.ts
+++ main.ts
@@ -1,165 +1,259 @@
-abstract class CarConstructor {
-    name: string;
+class Car {
     price: number;
-}
-
-class Car extends CarConstructor {
-    name: string;
+    model: string;
     color: string;
+    engine: object;
     doors: number;
-    engineType: string;
-    price: number;
-    enginePower: number;
-
-    // checking price for undefined, if it's undefined, we equals price to 0 to avoid NaN result for price
-    private checkPrice() {
-        if (this.price === undefined) this.price = 0
-    }
-
-    setEngine(engineType: string) {
-        this.checkPrice();
-
-        const engine = {
+    result: string;
+    constructor() {
+        this.price = 1000;
+        this.model = 'car';
+        this.color = 'color'
+        this.engine = {
             oil: {
-                power: 8,
-                price: 100,
+                type: 'oil',
+                power: 11
             },
             hybrid: {
-                power: 7,
-                price: 200,
+                type: 'hybrid',
+                power: 9
             }
         }
-
-        if (engineType === 'oil') {
-            this.enginePower = engine[engineType].power;
-            this.engineType = 'oil';
-            this.price += engine[engineType].price;
-        } else if(engineType === 'hybrid') {
-            this.enginePower = engine[engineType].power;
-            this.engineType = 'hybrid';
-            this.price += engine[engineType].price;
-        }
-
+        this.doors = 2;
+        this.result = ''
     }
-
-    getEngineInfo() {
-        console.log(`Engine type: ${this.engineType}\nEngine power: ${this.enginePower}`)
+    
+    getPrice() {
+        return this.price;
     }
-
-    getName() {
-        if (this.name) {
-            console.log(`Name: ${this.name}`)
-        } else {
-            console.log(`Please set car name using setName(name) method`)
-        }
-    }
-
-    setName(carName: string) {
-        this.name = carName;
+    
+    getModel() {
+        return this.model;
     }
 
     getColor() {
-        if (this.color) {
-            console.log(`Color: ${this.color}`)
-        } else {
-            console.log(`Please set car color using setColor(color) method.`)
-        }
+        return this.color;
     }
-
-    setColor(carColor: string) {
-        if (this.price === undefined) {
-            this.price = 0
-        }
-        const colorsPriceList = {
-            random: 1,
-            black: 5,
-            white: 5,
-            grey: 5,
-            yellow: 2,
-            purple: 3,
-            pink: 2,
-            else: 1,
-            gold: 6,
-            chamelion: 6,
-            blue: 4,
-            oceanBLue: 5,
-            green: 3,
-        }
-        if (carColor in colorsPriceList) {
-            this.price += colorsPriceList[carColor];
-        } else {
-            this.price += colorsPriceList['else'];
-        }
-        this.color = carColor;
+    getEngine() {
+        return this.engine
     }
-
+    getEnginePower() {
+        return this.engine
+    }
     getDoors() {
-        if (this.doors) {
-            console.log(`Doors count: ${this.doors}`)
-        } else {
-            console.log(`Please set doors count using setDoorsCount method`)
-        }
+        return this.doors
     }
-
-    setDoors(carDoorsCount: number) {
-        this.checkPrice();
-
-        if ( carDoorsCount === 2 || carDoorsCount === 4 ) {
-            this.doors = carDoorsCount;
-
-            if (carDoorsCount === 2) {
-                this.price += 2
-            } else if(carDoorsCount === 4){
-                this.price += 4
-            }
-        } else {
-            console.log('Please set normal count of doors (2 or 4)')
-        }
-    }
-
-    // checking every key property, if one of them (or more) did not define, it will log about it
-    protected checkAllKeyProps() {
-        if (this.doors && this.color && this.engineType && this.name) {
-            this.checkForMissingDetails();
-            return true
-        } else {
-            console.log(`
-                You are missed some of key configs or entered false values,please fill or correct them with 'set' methods.(setDoors, setColor, setEngine, setName)
-            `)
-            return false
-        }
-    }
-
-    // checking price and name properties, if one of them not defined it will log attention about checking some methods
-    protected checkForMissingDetails() {
-        if ( !(this.price && this.name) ) {
-            console.log(`Please configure car with 'set' methods to see the price that fits your selected parts.`)
-        }
-    }
-
-    logPrice() {
-        return this.price
-    }
-
-    // if checkAllKeyProps return es true it will show expected text
     getResult() {
-        if (this.checkAllKeyProps()){
-            console.log(`${this.name} whit color ${this.color} and price ${this.logPrice()}, and your car has a ${this.engineType} engine and ${this.enginePower} power`)
-        }
+        return this.result
     }
 
 }
 
-const bmw = new Car();
+class Tesla extends Car {
+    constructor() {
+        super();
+        this.price = 500;
+        this.model = 'tesla';
+        this.color = 'red';
+        this.engine = {
+            type: 'hybrid',
+            power: 9,
+        };
+        this.doors = 4;
+        this.result = 'result'
 
+    }
+}
+class Bmw extends Car {
+    constructor() {
+        super();
+        this.price = 300;
+        this.model = 'bmw';
+        this.color = 'white';
+        this.engine = {
+            type: 'oil',
+            power: 22,
+        };
+        this.doors = 2;
+        this.result = 'result'
 
-bmw.setName('BMW');
-bmw.getName();
-bmw.setColor('a');
-bmw.getColor();
-bmw.setDoors(2);
-bmw.getDoors();
-bmw.setEngine('hybrid');
-bmw.getEngineInfo();
+    }
+}
 
-bmw.getResult();
+class Model {
+    car: any;
+    constructor(car) {
+        this.car = car
+    }
+    getPrice() {
+        return this.car.getPrice()
+    }
+    getModel() {
+        return this.car.getModel()
+    }
+    getColor() {
+        return this.car.getColor()
+    }
+    getEngine() {
+        return this.car.getEngine()
+    }
+    getEnginePower() {
+        return this.car.getEngine()
+    }
+    getDoors() {
+        return this.car.getDoors()
+    }
+    getResult() {
+        return this.car.getResult()
+    }
+}
+
+class Color {
+    car: any;
+    constructor(car) {
+        this.car = car;
+    }
+
+    getPrice() {
+        return this.car.getPrice() + 100 // + color price
+    }
+    getModel() {
+            return this.car.getModel()
+    }
+    getColor() {
+        return this.car.getColor()
+    }
+    getEngine() {
+        return this.car.getEngine()
+    }
+    getEnginePower() {
+        return this.car.getEngine()
+    }
+    getDoors() {
+        return this.car.getDoors()
+    }
+    getResult() {
+        return this.car.getResult()
+    }
+
+}
+
+class Engine {
+    car: any;
+    constructor(car) {
+        this.car = car;
+    }
+
+    getPrice() {
+        return this.car.getPrice() + 500 // + engine price
+    }
+    getModel() {
+            return this.car.getModel()
+    }
+    getColor() {
+        return this.car.getColor()
+    }
+    getEngine() {
+        return this.car.getEngine()
+    }
+    getEnginePower() {
+        return this.car.getEngine()
+    }
+    getDoors() {
+        return this.car.getDoors()
+    }
+    getResult() {
+        return this.car.getResult()
+    }
+}
+
+class Doors {
+    car: any;
+    constructor(car) {
+        this.car = car;
+    }
+
+    getPrice() {
+        return this.car.getPrice() + 50 // + doors price
+    }
+    getModel() {
+        return this.car.getModel()
+    }
+    getColor() {
+        return this.car.getColor()
+    }
+    getEngine() {
+        return this.car.getEngine()
+    }
+    getEnginePower() {
+        return this.car.getEngine()
+    }
+    getDoors() {
+        return this.car.getDoors()
+    }
+    getResult() {
+        return this.car.getResult()
+    }
+}
+
+class Result {
+    car: any;
+    constructor(car) {
+        this.car = car;
+    }
+
+    getPrice() {
+        return this.car.getPrice()
+    }
+    getModel() {
+        return this.car.getModel()
+    }
+    getColor() {
+        return this.car.getColor()
+    }
+    getEngine() {
+        return this.car.getEngine()["type"]
+    }
+    getEnginePower() {
+        return this.car.getEngine()["power"]
+    }
+    getDoors() {
+        return this.car.getDoors()
+    }
+    getResult() {
+        return `${this.getModel()} with color ${this.getColor()}, and price ${this.getPrice()}, with ${this.getEngine()} engine.`
+    }
+
+}
+
+let tesla: any = new Tesla();
+tesla = new Model(tesla);
+tesla = new Color(tesla);
+tesla = new Engine(tesla);
+tesla = new Doors(tesla);
+tesla = new Result(tesla);
+
+console.log(tesla.getPrice());
+console.log(tesla.getModel());
+console.log(tesla.getColor());
+console.log(tesla.getEngine());
+console.log(tesla.getEnginePower());
+console.log(tesla.getDoors());
+console.log(tesla.getResult());
+
+console.log('----------------------------------------')
+
+let bmw: any = new Bmw();
+bmw = new Model(bmw);
+bmw = new Color(bmw);
+bmw = new Engine(bmw);
+bmw = new Doors(bmw);
+bmw = new Result(bmw);
+
+console.log(bmw.getPrice());
+console.log(bmw.getModel());
+console.log(bmw.getColor());
+console.log(bmw.getEngine());
+console.log(bmw.getEnginePower());
+console.log(bmw.getDoors());
+console.log(bmw.getResult());
